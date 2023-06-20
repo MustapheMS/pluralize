13097bd94ce3ba5bbb54851ccab9051057f61b38
MustapheMS <moztaphem9@gmail.com>
2023-06-20 21:21:28 +0300

    Pluralize Done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Good Luck 💪🏾
+
+//Done 😎
+function pluralize(words) {
+  var wordCount = {};
+  var pluralWords = [];
+
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    if (wordCount[word]) {
+      var pluralForm = word + 's';
+      if (!wordCount[pluralForm]) {
+        var singularIndex = pluralWords.indexOf(word);
+        pluralWords[singularIndex] = pluralForm; 
+        wordCount[pluralForm] = true;
+      }
+    } else {
+      wordCount[word] = true;
+      pluralWords.push(word); 
+    }
+  }
+  return pluralWords;
+}
+var words = ['ferrari', 'mercedez', 'suzuki', 'hyundi', 'suzuki', 'toyota','ferrari'];
+var pluralWords = pluralize(words);
+
+console.log(pluralWords);
